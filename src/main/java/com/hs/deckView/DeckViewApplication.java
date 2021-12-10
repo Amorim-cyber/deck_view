4252ee60ee52cae881568b739afb702e3a076e7e
Lucas Mattos Amorim <amorim22lucas@gmail.com>
2021-12-09 22:15:56 -0300

    begin

---
+++ src/main/java/com/hs/deckView/DeckViewApplication.java
@@ -0,0 +1,13 @@
+package com.hs.deckView;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeckViewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeckViewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hs/deckView/DeckViewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hs.deckView;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeckViewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
