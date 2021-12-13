f204e3d9e9495a5ef1d8509ae51f0554273501b1
Lucas Mattos Amorim <amorim22lucas@gmail.com>
2021-12-13 15:27:23 -0300

    add MessageUtils.java

---
+++ src/main/java/com/hs/deckView/util/MessageUtils.java
@@ -0,0 +1,9 @@
+package com.hs.deckView.util;
+
+public abstract class MessageUtils {
+
+    public static final String CARD_ALREADY_EXISTS = "Card already register in the database";
+
+    public static final String NO_RECORDS_FOUND = "No records found";
+
+}
