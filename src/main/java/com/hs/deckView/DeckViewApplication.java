d0afd92632264c95695a57b3f4adda8f77e9e705
Lucas Mattos Amorim <amorim22lucas@gmail.com>
2021-12-13 11:54:45 -0300

    update DeckViewApplication.java

--- src/main/java/com/hs/deckView/DeckViewApplication.java
+++ src/main/java/com/hs/deckView/DeckViewApplication.java
@@ -1,13 +1,27 @@
 package com.hs.deckView;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DeckViewApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DeckViewApplication.class, args);
+	}
+
+	@Bean
+	public OpenAPI customOpenAPI(@Value("${application.description}") String description){
+		return new OpenAPI().info(new Info()
+				.title(description)
+				.version("1.0")
+				.termsOfService("http://swagger.io/terms")
+				.license(new License().name("Apache 2.0").url("http://springdoc.org")));
 	}
 
 }
